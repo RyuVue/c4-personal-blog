1f65cd8a0546c09ba4913e8d2ccf476c27a1c7bc
Ryu Vue <ryuvwj@gmail.com>
2024-06-01 08:04:10 -0500

    added a back button to blog page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,7 @@
 document.getElementById('modeToggle').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 })
+
+document.getElementById('backButton').addEventListener('click', function() {
+    window.location.href = 'index.html';
+  });
